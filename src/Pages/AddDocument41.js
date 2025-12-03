8c57f3c2924ee807e617832849589ff80062816d
mostafa213 <win 10@DESKTOP-5V6PD88>
2025-12-03 14:36:22 +0200

    adding model to doc41

--- src/Pages/AddDocument41.js
+++ src/Pages/AddDocument41.js
@@ -24,15 +24,20 @@ const AddDocument41 = () => {
     [t]
   );
 
+  const [arrTransactionNature, setArrTransactionNature] = useState([]);
+  const [strFiscalYear, setStrFiscalYear] = useState("");
   const [objSupplier, setObjSupplier] = useState(null);
   const [objItem, setObjItem] = useState(null);
   const [objDocument41, setObjDocument41] = useState({
+    serialNumber: "",
     item: null,
     supplierId: null,
     transactionDate: "",
-    fiscalYearId: null,
+    fiscalYearId: -1,
     transactionNatureId: -1,
-    amount: 0,
+    amount: 1,
+    taxPercent: 14,
+    price: 0
   });
   const strDocDir = document.documentElement.dir;
 
@@ -51,7 +56,6 @@ const AddDocument41 = () => {
         value: x.id,
         objItem: x
       }));
-      console.log(arr);
       return arr;
   };
   const arrItem = async (strInput) => {
@@ -64,9 +68,9 @@ const AddDocument41 = () => {
     const res = await axiosInstance.post("Item/ListAll", objFilter);
     let arr = res.data.map(x => ({
       label: x.name,
-      value: x.id
+      value: x.id,
+      objItem: x
     }));
-    console.log(arr);
     return arr;
   };
   const breadcrumbItems = [
@@ -74,8 +78,31 @@ const AddDocument41 = () => {
     { label: t("Add"), link: "", active: true }
   ];
 
+  const UpdateFiscalYear = (strDate) => {
+    // featsh fiscal year based on date
+    let date = new Date(strDate);
+    let year = date.getFullYear();
+    setObjDocument41(prev => ({...prev, fiscalYearId: year}));
+    setStrFiscalYear(year.toString());
+  }
+
+  const Add = async () => {
+    let response = await axiosInstance.post("Document41/Add", objDocument41)
+    if (response.data.result) {
+      alert(response.data.message);
+    }
+  }
+
+  const listTransactionNature = async () => {
+    const res = await axiosInstance.post("TransactionNature/ListAll",{});
+    if (res.data.result) {
+      setArrTransactionNature(res.data.data);
+    }
+  }
+
   useEffect(() => {
     // fetch data here if needed
+    listTransactionNature();
   }, []);
 
   return (
@@ -96,24 +123,31 @@ const AddDocument41 = () => {
               defaultOptions={false}
               loadOptions={arrSupplier}
               value={objSupplier}
-              onChange={(option) => setObjSupplier(option)}
+              onChange={(option) => {
+                  setObjSupplier(option);
+                  setObjDocument41(prev => ({ ...prev, supplierId: option.value }));
+                }
+              }
             />
           </div>
           <div className="col-md-6 form-group">
             <label>{t("Transaction Date")}</label>
-            <input type="date" className="mt-2 form-control" />
+            <input type="date" className="mt-2 form-control" value={objDocument41.transactionDate} onChange={(e) => { setObjDocument41((prev) => ({...prev, transactionDate: e.target.value})); UpdateFiscalYear(e.target.value);}} />
           </div>
         </div>
 
         <div className="row p-4">
           <div className="col-md-6 form-group">
             <label>{t("Fiscal Year")}</label>
-            <label className="mt-2 form-control">{t("Fiscal Year")}</label>
+            <label className="mt-2 form-control">{strFiscalYear ?? t("Fiscal Year")}</label>
           </div>
           <div className="col-md-6 form-group">
             <label>{t("Transaction Nature")}</label>
-            <select className="mt-2 form-control">
-              <option >{t("Transaction Nature")}</option>
+            <select className="mt-2 form-control" value={objDocument41.transactionNatureId} onChange={(e) => setObjDocument41((prev) => ({...prev, transactionNatureId: Number(e.target.value)}))}>
+              <option value="-1">{t("Transaction Nature")}</option>
+              {arrTransactionNature.map((nature) => (
+                <option key={nature.id} value={nature.id}>{nature.name}</option>
+              ))}
             </select>
           </div>
         </div>
@@ -127,31 +161,38 @@ const AddDocument41 = () => {
               defaultOptions={false}
               loadOptions={arrItem}
               value={objItem}
-              onChange={(option) => setObjItem(option.objItem)}
+              onChange={(option) => {
+                setObjItem(option);
+                setObjDocument41(prev => ({...prev, item: option.value}));
+                setObjDocument41(prev => ({...prev, price: option.objItem?.price || 0}));
+              }}
             />
           </div>
-          <div className="col-md-3 form-group">
+          <div className="col-md-2 form-group">
             <label>{t("Price")}</label>
-            <input type="number" className="mt-2 form-control" placeholder="0.0" />
+            <input type="number" className="mt-2 form-control" placeholder="0.0" value={objDocument41.price} onChange={(e) => {setObjDocument41(prev => ({...prev, price: Number(e.target.value * objDocument41.amount)}))}} />
           </div>
-          <div className="col-md-3 form-group">
+          <div className="col-md-2 form-group">
+            <label>{t("Tax")}</label>
+            <input type="number" className="mt-2 form-control" value={objDocument41.taxPercent} onChange={(e) => setObjDocument41(prev => ({...prev, taxPercent: Number(e.target.value)}))} />
+          </div>
+          <div className="col-md-2 form-group">
             <label>{t("Amount")}</label>
-            <input type="number" className="mt-2 form-control" placeholder="0" />
+            <input type="number" className="mt-2 form-control" placeholder="1" value={objDocument41.amount} onChange={(e) => setObjDocument41(prev => ({...prev, amount: Number(e.target.value)}))}disabled />
           </div>
         </div>
 
         <div className="row p-4" dir={strDocDir === "rtl" ? "ltr" : "rtl"}>
           <div className="col-md-2 pull-left border border-black me-3" dir={strDocDir}>
-            <strong>{t("Total Amount")} : </strong><span>{"0.0"}</span><br />
-            <strong>{t("Tax Percent")} : </strong><span>{"0.0"} %</span><br />
-            <strong>{t("Tax Amount")} : </strong><span>{"0.0"}</span><br />
-            <strong>{t("Net Amount")} : </strong><span>{"0.0"}</span><br />
+            <strong>{t("Total Amount")} : </strong><span>{objDocument41.price ?? "0.0"}</span><br />
+            <strong>{t("Tax Percent")} : </strong><span>{objDocument41.taxPercent ?? "0"} %</span><br />
+            <strong>{t("Tax Amount")} : </strong><span>{((objDocument41.price * objDocument41.taxPercent) / 100) ?? "0.0"}</span><br />
           </div>
         </div>
 
         <div className="row p-4" dir={strDocDir === "rtl" ? "ltr" : "rtl"}>
           <div className="col-md-3 me-2">
-            <button type="button" className="btn btn-success">{t("Add")}</button>
+            <button type="button" className="btn btn-success" onClick={Add()}>{t("Add")}</button>
           </div>
         </div>
       </div>
